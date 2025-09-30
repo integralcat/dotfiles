399ad5e264f9e091d0773f807179d9588d33b099
c2-07 <114809705+C2-07@users.noreply.github.com>
2025-09-30 17:46:26 +0530

    Cleaned up nvim and fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,15 @@
 # === ENV VARS ===
 export HOMEBREW_NO_ENV_HINTS=1
 export RUSTFLAGS="-C linker=clang -C link-arg=-fuse-ld=lld"
-export DOTLINK_ROOT="$HOME/dotfiles/"
+
+# Show all warnings and treat them as error
+export CFLAGS="clang -std=c99 -Wall -Werror"
 
 set -x MANPAGER bat
-set -gx PATH $HOME/.cabal/bin $PATH $HOME/.ghcup/bin
 
 # === PATH SETUP ===
-set -p PATH ~/.platformio/penv/bin
 set -p PATH ~/.local/bin
-set -p PATH ~/.pub-cache/bin
-set -p PATH ~/.emacs.d/bin
-set -p PATH $FLYCTL_INSTALL/bin
-set -p PATH "$HOME/.local/share/gem/ruby/3.4.0/bin"
+set -p PATH /opt/homebrew/bin
 
 # === GREETING + PROMPT ===
 set fish_greeting
@@ -21,13 +18,8 @@ set VIRTUAL_ENV_DISABLE_PROMPT 1
 # === ZOXIDE ===
 zoxide init fish | source
 
-# === FASTFETCH (macOS specific) ===
-if test "$TERM" = xterm-kitty
-    fastfetch --kitty-direct "$HOME/.config/fastfetch/shell_logo.png"
-else if status --is-interactive
-    if test "$TERM_PROGRAM" != vscode
-        fastfetch -l none
-    end
+function fish_greeting
+    fastfetch -l none
 end
 
 # === ALIASES ===
@@ -36,12 +28,12 @@ alias so='source ~/dotfiles/fish/config.fish'
 alias fish_config="nvim ~/.config/fish/config.fish"
 
 # File utilities
-alias ls='exa -l --color=always --group-directories-first --icons=always --no-time --no-user '
-alias la='exa -a --color=always --group-directories-first --icons'
-alias ll='exa -l --color=always --group-directories-first --icons'
-alias lt='exa -aT --color=always --group-directories-first --icons'
-alias l.="exa -a | egrep '^\.'"
-alias cat='bat'
+alias ls='eza -l --color=always --group-directories-first --icons=always --no-time --no-user '
+alias la='eza -a --color=always --group-directories-first --icons'
+alias ll='eza -l --color=always --group-directories-first --icons'
+alias lt='eza -aT --color=always --group-directories-first --icons'
+alias l.="eza -a | egrep '^\.'"
+alias cat='bat -p'
 alias tree='eza --tree'
 
 # Text & Terminal
@@ -64,18 +56,13 @@ alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
 
 # Tools
-alias helix='hx'
 alias py="python3"
-alias vfzf='nvim "$(fzf)"'
-alias ssh_phone="ssh -p 8022 u0_a272@192.168.0.101"
+alias neof='nvim "$(fzf)"'
 
 # Package managers (adapt for macOS)
 alias xi="brew install"
 alias xr="brew uninstall"
 alias xq="brew info"
-
-# Different Profiles for NVIM 
-alias nesx-vi="NVIM_APPNAME=dotfiles/nvim-instances/nesx-vi nvim"
 
 # YouTube
 alias download_song="yt-dlp -x --audio-format mp3 --embed-thumbnail"
@@ -93,9 +80,9 @@ alias at='aria2c --dir=downloads/ --seed-time=0'
 alias am='aria2c --bt-tracker=udp://tracker.openbittorrent.com:80,udp://tracker.opentrackr.org:1337/announce '
 
 # Misc
-alias tarnow='tar -acf '
-alias untar='tar -xvf '
-alias wget='wget -c '
+alias tarnow='tar -acf'
+alias untar='tar -xvf'
+alias wget='wget -c'
 alias psmem='ps aux | sort -nrk 4'
 alias psmem10='ps aux | sort -nrk 4 | head -10'
 
@@ -123,13 +110,13 @@ function __history_previous_command_arguments
 end
 
 # Binding bang-bang support
-if [ "$fish_key_bindings" = fish_vi_key_bindings ]
-    bind -Minsert ! __history_previous_command
-    bind -Minsert '$' __history_previous_command_arguments
-else
-    bind ! __history_previous_command
-    bind '$' __history_previous_command_arguments
-end
+# if [ "$fish_key_bindings" = fish_vi_key_bindings ]
+#     bind -Minsert ! __history_previous_command
+#     bind -Minsert '$' __history_previous_command_arguments
+# else
+#     bind ! __history_previous_command
+#     bind '$' __history_previous_command_arguments
+# end
 
 # Better `cp`
 function copy
@@ -186,6 +173,8 @@ function ydownload
         $url
 end
 
-# === DONE NOTIFICATIONS ===
-set -U __done_min_cmd_duration 10000
-set -U __done_notification_urgency_level low
+function fish_mode_prompt
+end
+
+# Hydro
+set --global hydro_multiline true
