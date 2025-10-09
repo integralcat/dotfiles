5a51d6e5c0efe31978ce8959bbda5a9a581c76b9
c2-07 <114809705+C2-07@users.noreply.github.com>
2025-10-10 02:25:01 +0530

    Deleting old nvim && Creating new branch for neovim configuration and testing

--- nvim/lua/plugins/vim-move.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "matze/vim-move",
-    event = "VeryLazy",
-    init = function()
-        -- Optional remaps (uncomment if you want to change defaults)
-        -- vim.g.move_key_modifier = "C"  -- Change Alt to Ctrl
-        -- vim.g.move_map_keys = 0       -- Disable default keybindings if you want custom ones
-    end,
-}
