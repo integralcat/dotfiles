59a4036681760fbb374f328751f74821221b37c2
GouraV <gourav@GouraVs-MacBook-Air.local>
2025-11-18 02:13:04 +0530

    Update fish config and added backup for rectangle config

--- fish/config.fish
+++ fish/config.fish
@@ -1,181 +1,158 @@
-# === ENV VARS ===
-export HOMEBREW_NO_ENV_HINTS=1
-export RUSTFLAGS="-C linker=clang -C link-arg=-fuse-ld=lld"
+# env vars
+set -gx HOMEBREW_NO_ENV_HINTS 1
+set -gx RUSTFLAGS "-C linker=clang -C link-arg=-fuse-ld=lld"
+set -gx CFLAGS "clang -std=c99 -Wall -Werror"
+set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
+set -gx CARGO_TARGET_DIR "$HOME/.cargo-target"
 
-# Show all warnings and treat them as error
-export CFLAGS="clang -std=c99 -Wall -Werror"
-
-set -x MANPAGER bat
-
-# === PATH SETUP ===
-set -p PATH ~/.local/bin
-set -p PATH /opt/homebrew/bin
-set -p PATH ~/.rustup/toolchains/stable-aarch64-apple-darwin/bin/
-
-# === GREETING + PROMPT ===
-set fish_greeting
-set VIRTUAL_ENV_DISABLE_PROMPT 1
-
-# === ZOXIDE ===
-zoxide init fish | source
-
-function fish_greeting
-    ~/.local/bin/nanofetch
+# safe PATH adding
+function __safe_add_path
+    for p in $argv
+        if test -d $p
+            set -g PATH $p $PATH
+        end
+    end
 end
 
-# === ALIASES ===
-alias cls='clear'
-alias so='source ~/dotfiles/fish/config.fish'
+__safe_add_path ~/.local/bin
+__safe_add_path /opt/homebrew/bin
+__safe_add_path ~/.rustup/toolchains/stable-aarch64-apple-darwin/bin
+
+# starship (only if installed)
+if type -q starship
+    starship init fish --print-full-init | source
+    set -gx STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
+end
+
+# manpager
+if type -q bat
+    set -gx MANPAGER bat
+end
+
+# greeting
+set fish_greeting
+function fish_greeting
+    if test -x ~/.local/bin/nanofetch
+        ~/.local/bin/nanofetch
+    end
+end
+
+# safe alias wrapper
+function __safe_alias
+    if type -q $argv[2]
+        alias $argv[1]="$argv[2]"
+    end
+end
+
+# basic aliases
+alias cls="clear"
+alias so="source ~/dotfiles/fish/config.fish"
 alias fish_config="nvim ~/.config/fish/config.fish"
 
-# File utilities
-alias ls='eza -l --color=always --group-directories-first --icons=always --no-time --no-user '
-alias la='eza -a --color=always --group-directories-first --icons'
-alias ll='eza -l --color=always --group-directories-first --icons'
-alias lt='eza -aT --color=always --group-directories-first --icons'
-alias l.="eza -a | egrep '^\.'"
-alias cat='bat -p'
-alias tree='eza --tree'
+# ls / tree
+if type -q eza
+    alias ls="eza -l --color=always --group-directories-first --icons=always --no-time --no-user"
+    alias la="eza -a --color=always --group-directories-first --icons"
+    alias ll="eza -l --color=always --group-directories-first --icons"
+    alias lt="eza -aT --color=always --group-directories-first --icons"
+    alias tree="eza --tree"
+else
+    alias ls="ls -lah"
+end
 
-# Text & Terminal
-alias man='batman'
-alias export='set -x VAR'
+# cat
+if type -q bat
+    alias cat="bat -p"
+end
 
-# Git
-alias g="git"
+# git
+__safe_alias g git
 alias gs="git status"
 alias gc="git commit -m"
 alias gp="git push"
-alias dotfiles='/usr/bin/git --git-dir=$HOME/dotfiles '
 
-# Navigation
-alias cd='z'
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
-
-# Tools
-alias py="python3"
-alias neof='nvim "$(fzf)"'
-
-# Package managers (adapt for macOS)
-alias xi="brew install"
-alias xr="brew uninstall"
-alias xq="brew info"
-
-# YouTube
-alias download_song="yt-dlp -x --audio-format mp3 --embed-thumbnail"
-
-# Compilers
-alias gcc="gcc -Wall -Wextra"
-alias g++="g++ -Wall -Wextra"
-
-# Search + File ops
-alias grep='rg'
-alias find='fd'
-
-# Torrenting and Downloading with Aria2
-alias at='aria2c --dir=downloads/ --seed-time=0'
-alias am='aria2c --bt-tracker=udp://tracker.openbittorrent.com:80,udp://tracker.opentrackr.org:1337/announce '
-
-# Misc
-alias tarnow='tar -acf'
-alias untar='tar -xvf'
-alias wget='wget -c'
-alias psmem='ps aux | sort -nrk 4'
-alias psmem10='ps aux | sort -nrk 4 | head -10'
-
-# === FUNCTIONS ===
-
-# History support for !! and !$ (Fish version)
-function __history_previous_command
-    switch (commandline -t)
-        case "!"
-            commandline -t $history[1]
-            commandline -f repaint
-        case "*"
-            commandline -i !
-    end
+# navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+if type -q z
+    alias cd="z"
 end
 
-function __history_previous_command_arguments
-    switch (commandline -t)
-        case "!"
-            commandline -t ""
-            commandline -f history-token-search-backward
-        case "*"
-            commandline -i '$'
-    end
+# search tools
+__safe_alias grep rg
+__safe_alias find fd
+
+# brew
+if type -q brew
+    alias xi="brew install"
+    alias xr="brew uninstall"
+    alias xq="brew info"
 end
 
-# Binding bang-bang support
-# if [ "$fish_key_bindings" = fish_vi_key_bindings ]
-#     bind -Minsert ! __history_previous_command
-#     bind -Minsert '$' __history_previous_command_arguments
-# else
-#     bind ! __history_previous_command
-#     bind '$' __history_previous_command_arguments
-# end
+# yt-dlp
+if type -q yt-dlp
+    alias download_song="yt-dlp -x --audio-format mp3 --embed-thumbnail"
+end
 
-# Better `cp`
+# file copy
 function copy
     set count (count $argv | tr -d \n)
     if test "$count" = 2; and test -d "$argv[1]"
-        set from (echo $argv[1] | trim-right /)
-        set to (echo $argv[2])
-        command cp -r $from $to
+        command cp -r (string trim -r -c '/') $argv[1] $argv[2]
     else
         command cp $argv
     end
 end
 
-# Make a backup of file
-function backup --argument filename
-    cp $filename $filename.bak
+# backup
+function backup --argument file
+    if test -f $file
+        cp $file $file.bak
+    else
+        echo "file not found: $file"
+    end
 end
 
-function yplay
-    set -l query (read -P "Search YouTube: ")
-    if test -z "$query"
-        echo "No search query given."
-        return
+# yplay
+if type -q mpv; and type -q yt-dlp; and type -q fzf
+    function yplay
+        set -l q (read -P "Search YouTube: ")
+        test -z "$q"; and echo "no query"; and return
+
+        set -l url (yt-dlp "ytsearch10:$q" --flat-playlist \
+            --print "%(title)s | https://youtu.be/%(id)s" | fzf | cut -d'|' -f2 | string trim)
+
+        test -z "$url"; and echo "no selection"; and return
+
+        mpv --no-video (yt-dlp -f bestaudio -g $url)
     end
-    set -l url (yt-dlp "ytsearch10:$query" --flat-playlist --print "%(title)s | https://youtu.be/%(id)s" | fzf | cut -d' | ' -f2 | string trim)
-    if test -z "$url"
-        echo "No selection made."
-        return
-    end
-    mpv --no-video (yt-dlp -f bestaudio -g $url)
 end
 
-function ydownload
-    set -l query (read -P "Search YouTube: ")
-    if test -z "$query"
-        echo "No search query given."
-        return
+# ydownload
+if type -q yt-dlp; and type -q fzf
+    function ydownload
+        set -l q (read -P "Search YouTube: ")
+        test -z "$q"; and echo "no query"; and return
+
+        set -l url (yt-dlp "ytsearch10:$q" --flat-playlist \
+            --print "%(title)s | https://youtu.be/%(id)s" | fzf | cut -d'|' -f2 | string trim)
+
+        test -z "$url"; and echo "no selection"; and return
+
+        echo "Downloading: $url"
+        yt-dlp -f bestaudio --extract-audio --audio-format mp3 \
+            --audio-quality 0 --embed-thumbnail --add-metadata \
+            --output "$HOME/Downloads/%(title)s.%(ext)s" $url
     end
-    set -l url (yt-dlp "ytsearch10:$query" --flat-playlist --print "%(title)s | https://youtu.be/%(id)s" | fzf | cut -d' | \
-' -f2 | string trim)
-    if test -z "$url"
-        echo "No selection made."
-        return
-    end
-    echo "Downloading audio from: $url"
-    yt-dlp \
-        -f bestaudio \
-        --extract-audio \
-        --audio-format mp3 \
-        --audio-quality 0 \
-        --embed-thumbnail \
-        --add-metadata \
-        --output "$HOME/Downloads/%(title)s.%(ext)s" \
-        $url
 end
 
-function fish_mode_prompt
+# zoxide
+if type -q zoxide
+    zoxide init fish | source
 end
 
-# Hydro
-set --global hydro_multiline true
+# hydro
+if set -q hydro_multiline
+    set -g hydro_multiline true
+end
